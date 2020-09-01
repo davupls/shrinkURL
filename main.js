7dd5551aaa23d7cc71c1c596c0b9730b4bb0b47b
David Mclean <davidmiltonmclean@gmail.com>
2020-09-01 17:02:40 -0500

    Improved JS Func's
    Better JS Code layout and flow.
    Now the code can post data onto the page, but needs more work.

--- main.js
+++ main.js
@@ -1,33 +1,64 @@
 const btn = document.querySelector(".url-form__btn");
-const urlValue = document.querySelector(".url-form__input");
+const input = document.querySelector(".url-form__input");
 const resData = document.querySelector(".resData__h1")
 
-let value = urlValue.value;   
-const url = "https://rel.ink/api/links/";
-const link = {
-        "url" : "https://www.youtube.com/watch?v=_KbFNKWdnvE"
+
+
+btn.addEventListener("click", checkInput);
+
+function checkInput(){
+    let userInputValue = input.value;
+    let link = { "url" : `${userInputValue}`}
+    const url = "https://rel.ink/api/links/";
+
+    // Fetch Data Method Settings
+    const settings = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(link),
+    }
+    async function fetchData() {
+        const response = await fetch(url, settings);
+        const data = await response.json();
+        console.log(data.hashid)
+
+        function placeData(){
+            const newURL = url + data.hashid;
+            let newDiv = document.createElement("h2")
+            let infoDiv = document.createTextNode(`${data.hashid}`);
+
+            newDiv.appendChild(infoDiv)
+            const currentEle = document.querySelector(".resData"); 
+            document.body.insertBefore(newDiv, currentEle);
+            resData.innerHTML = url + data.hashid;
+        }
+
+        placeData();
     }
 
-const settings = {
-    method: 'POST',
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(link),
-}
+    
 
-async function fetchData(urlVariable) {
-    const response = await fetch(urlVariable, settings);
-    const data = await response.json();
-
-    function placeData(){
-        console.log(data.hashid + " working")
-        resData.innerHTML = url + data.hashid;
+    if ( userInputValue === "" ) {
+        console.log("Please enter a url, input field is empty.")
+    } else {
+        console.log("Thank you for data!: ")
+        fetchData();
     }
-
-    placeData();
-    return data.hashid;
 }
 
-fetchData(url);
+
+// async function fetchData() {
+//     const response = await fetch(url, settings);
+//     const data = await response.json();
+
+//     function placeData(){
+//         console.log(data.hashid + " working")
+//         resData.innerHTML = url + data.hashid;
+//     }
+
+//     
+//     return data.hashid;
+// }
